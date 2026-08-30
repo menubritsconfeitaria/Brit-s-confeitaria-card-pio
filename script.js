3241e4e3e89bc6fb7767cf254e1dc3e8e6eff7e8
britsconfeitaria.com.br <georgevb89@gmail.com>
2026-08-30 15:46:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1939,10 +1939,11 @@ function fecharBoasVindas() {
 // pula a tela de boas-vindas e já abre a seção de personalização sozinha, expandida.
 // Ex: https://menubritsconfeitaria.github.io/Brit-s-confeitaria-card-pio/?venda=1
 const veioPeloLinkDeVenda = new URLSearchParams(window.location.search).get('venda') === '1';
+const veioPeloLinkDeProduto = new URLSearchParams(window.location.search).get('produto') != null;
 let scrollParaVendaPendente = veioPeloLinkDeVenda; // vira false depois de rolar uma vez
 let scrollParaProdutoPendente = true; // vira false depois da primeira tentativa de rolar pro produto do link
 
-if (veioPeloLinkDeVenda) {
+if (veioPeloLinkDeVenda || veioPeloLinkDeProduto) {
     try { sessionStorage.setItem('boasVindasBritS', '1'); } catch (e) { /* ignora */ } // pula a tela de boas-vindas
 } else {
     mostrarBoasVindas();
